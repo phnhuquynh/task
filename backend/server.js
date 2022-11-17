af1c4b34967a9aa2f47eef20b4029d3ff2855e6b
phnhuquynh <phnhuquynh98@gmail.com>
2022-11-17 11:07:23 +0700

    3

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const connectDB=require("./config/connectDB");
-
+const dotenv=require ("dotenv").config();
 const app = express();
-
-
 
 app.get("/", (req,res)=>{
     res.send("Homepage");
@@ -14,4 +12,5 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
 })
+
 
